3a5ceee93764c642347b3cafe35bf8ec89c3d141
Gerald Baier <gerald.baier@tum.de>
2015-08-24 17:01:38 +0200

    rescale testing for full covariance matrix

--- src/compute/nlsar/cl_wrapper/covmat_rescale_test.cpp
+++ src/compute/nlsar/cl_wrapper/covmat_rescale_test.cpp
@@ -22,16 +22,17 @@ TEST_CASE( "covmat_rescale", "[cl_kernels]" ) {
 
         const float gamma = std::pow(std::min(float(nlooks)/float(dimension), 1.0f), 0.333333);
 
-        std::vector<float> inout_put      (height*width*dimension*dimension, 1.0);
-        std::vector<float> desired_output (height*width*dimension*dimension, 1.0);
+        std::vector<float> inout_put      (2*height*width*dimension*dimension, 1.0);
+        std::vector<float> desired_output (2*height*width*dimension*dimension, 1.0);
 
         for(int h = 0; h < height; h++) {
             for(int w = 0; w < width; w++) {
                 for(int row_idx = 0; row_idx < dimension; row_idx++) {
                     for(int col_idx = 0; col_idx < dimension; col_idx++) {
                         if (row_idx != col_idx) {
-                            const int idx = (row_idx * dimension + col_idx)*height*width + h*width + w;
-                            desired_output[idx] = gamma * inout_put[idx];
+                            const int idx = 2*(row_idx * dimension + col_idx)*height*width + h*width + w;
+                            desired_output[idx]              = gamma * inout_put[idx];
+                            desired_output[idx + height*width] = gamma * inout_put[idx + height*width];
                         }
                     }
                 }
@@ -51,7 +52,7 @@ TEST_CASE( "covmat_rescale", "[cl_kernels]" ) {
         covmat_rescale KUT{block_size, context};
 
         // allocate memory
-        cl::Buffer device_inout_put  {context, CL_MEM_READ_ONLY  | CL_MEM_COPY_HOST_PTR, height*width*dimension*dimension*sizeof(float), inout_put.data(), NULL};
+        cl::Buffer device_inout_put  {context, CL_MEM_READ_ONLY  | CL_MEM_COPY_HOST_PTR, 2*height*width*dimension*dimension*sizeof(float), inout_put.data(), NULL};
 
         KUT.run(cmd_queue, 
                 device_inout_put,
@@ -60,11 +61,11 @@ TEST_CASE( "covmat_rescale", "[cl_kernels]" ) {
                 height,
                 width);
 
-        cmd_queue.enqueueReadBuffer(device_inout_put, CL_TRUE, 0, height*width*dimension*dimension*sizeof(float), inout_put.data(), NULL, NULL);
+        cmd_queue.enqueueReadBuffer(device_inout_put, CL_TRUE, 0, 2*height*width*dimension*dimension*sizeof(float), inout_put.data(), NULL, NULL);
 
         // workaround, since Approx does not work with vectors
         bool flag = true;
-        for(int i = 0; i < height*width*dimension*dimension; i++) {
+        for(int i = 0; i < 2*height*width*dimension*dimension; i++) {
             flag = flag && (inout_put[i] == Approx(desired_output[i]).epsilon( 0.0001 ));
         }
         REQUIRE( ( flag ) );
