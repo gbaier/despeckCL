3956795ceb31e71dfdd79e1fe0184fb052221169
Gerald Baier <gerald.baier@tum.de>
2015-09-20 18:18:30 +0200

    speed up best_weights_copy

--- src/compute/nlsar/best_weights_copy.cpp
+++ src/compute/nlsar/best_weights_copy.cpp
@@ -7,12 +7,17 @@ std::vector<float> nlsar::best_weights_copy(std::map<params, std::vector<float>>
                                             const int search_window_size)
 {
     std::vector<float> best_weights (search_window_size*search_window_size*height*width);
+    std::map<params, float*> mapptr;
+    for(auto& x : weights) {
+        mapptr[x.first] = x.second.data();
+    }
     for(int h = 0; h < height; h++) {
         for(int w = 0; w < width; w++) {
-            const int offset = height*width;
             const params best_params = best_parameters[h*width + w];
+            float* best_match = mapptr.at(best_params);
             for(int i = 0; i < search_window_size*search_window_size; i++) {
-                best_weights[i*offset + h*width + w] = weights[best_params][i*offset + h*width + w];
+                const int idx = i*height*width + h*width + w;
+                best_weights[idx] = best_match[idx];
             }
         }
     }
