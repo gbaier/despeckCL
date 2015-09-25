ae470657a03295d407088cb5b5812d570c5cc34f
Gerald Baier <gerald.baier@tum.de>
2015-09-25 10:06:31 +0200

    speed up for map lookup

--- src/compute/nlsar/best_alpha_copy.cpp
+++ src/compute/nlsar/best_alpha_copy.cpp
@@ -6,10 +6,14 @@ std::vector<float> nlsar::best_alpha_copy(std::map<params, std::vector<float>> &
                                           const int width)
 {
     std::vector<float> best_alphas (height*width);
+    std::map<params, float*> mapptr;
+    for(auto& x : alphas) {
+        mapptr[x.first] = x.second.data();
+    }
     for(int h = 0; h < height; h++) {
         for(int w = 0; w < width; w++) {
             const params best_params = best_parameters[h*width + w];
-            best_alphas[h*width + w] = alphas[best_params][h*width + w];
+            best_alphas[h*width + w] = mapptr[best_params][h*width + w];
         }
     }
     return best_alphas;
