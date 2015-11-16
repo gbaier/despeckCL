422fcc28e990b37e910594b603fc0cab8570477a
Gerald Baier <gerald.baier@tum.de>
2015-11-16 09:52:59 +0100

    fixed boundary checks

--- src/data/sub_images.cpp
+++ src/data/sub_images.cpp
@@ -1,8 +1,6 @@
 #include "sub_images.h"
-#include <iostream>
 
 #include <stdlib.h>
-
 
 float* get_sub_image(const float * image,
                      const int height,
@@ -14,8 +12,8 @@ float* get_sub_image(const float * image,
     float* sub_image = (float *) malloc(sub_img_size*sub_img_size*sizeof(float));
     for(int h = 0; h < sub_img_size; h++) {
         for(int w = 0; w < sub_img_size; w++) {
-            const int h_img = std::max(height-1, std::min(0, h+h_low));
-            const int w_img = std::max(width-1,  std::min(0, w+w_low));
+            const int h_img = std::min(height-1, std::max(0, h+h_low));
+            const int w_img = std::min(width-1,  std::max(0, w+w_low));
             sub_image[h * sub_img_size + w] = image[h_img*width + w_img];
         }
     }
@@ -33,8 +31,8 @@ void write_sub_image(float * image,
 {
     for(int h = overlap; h < sub_img_size-overlap; h++) {
         for(int w = overlap; w < sub_img_size-overlap; w++) {
-            const int h_img = std::max(height-1, std::min(0, h+h_low));
-            const int w_img = std::max(width-1,  std::min(0, w+w_low));
+            const int h_img = std::min(height-1, std::max(0, h+h_low));
+            const int w_img = std::min(width-1,  std::max(0, w+w_low));
             image[h_img*width + w_img] = sub_image[h*sub_img_size + w];
         }
     }
