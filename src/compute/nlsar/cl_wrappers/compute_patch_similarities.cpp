020bf99f4a65c76a7950a74d321afd9ce21dd6c0
Gerald Baier <gerald.baier@tum.de>
2016-01-14 12:07:28 +0100

    better naming of parameters

--- src/compute/nlsar/cl_wrappers/compute_patch_similarities.cpp
+++ src/compute/nlsar/cl_wrappers/compute_patch_similarities.cpp
@@ -39,32 +39,36 @@ std::string nlsar::compute_patch_similarities::build_opts(void)
 void nlsar::compute_patch_similarities::run(cl::CommandQueue cmd_queue,
                                             cl::Buffer pixel_similarities,
                                             cl::Buffer patch_similarities,
-                                            const int height_sim,
-                                            const int width_sim,
+                                            const int height_pix,
+                                            const int width_pix,
                                             const int search_window_size,
                                             const int patch_size,
                                             const int patch_size_max)
 {
-    const int width_ori  = width_sim  - patch_size_max + 1;
-    const int height_ori = height_sim - patch_size_max + 1;
     const int offset = (patch_size_max - patch_size) / 2;
+
+    const int height_intermed = height_pix - 2*offset;
+    const int width_intermed  = width_pix  - 2*offset - patch_size + 1;
+
+    const int height_pat = height_intermed - patch_size + 1;
+    const int width_pat  = width_intermed;
 
     cl::NDRange local_size  {block_size_x, block_size_y, 1};
 
-    cl::Buffer intermed_row_conv {context, CL_MEM_READ_WRITE, search_window_size * search_window_size * (height_sim - 2*offset) * width_ori * sizeof(float), NULL, NULL};
+    cl::Buffer intermed_row_conv {context, CL_MEM_READ_WRITE, search_window_size * search_window_size * height_intermed * width_intermed * sizeof(float), NULL, NULL};
 
     kernel_row_pass.setArg(0, pixel_similarities);
     kernel_row_pass.setArg(1, intermed_row_conv);
-    kernel_row_pass.setArg(2, height_sim);
-    kernel_row_pass.setArg(3, width_sim);
+    kernel_row_pass.setArg(2, height_pix);
+    kernel_row_pass.setArg(3, width_pix);
     kernel_row_pass.setArg(4, patch_size);
     kernel_row_pass.setArg(5, patch_size_max);
     // +2 is for the left and right halo.
     // This code only works if the half of the patch size is smaller than block_size_x
     kernel_row_pass.setArg(6, cl::Local((block_size_x*(steps_row+2)) * block_size_y * sizeof(float)));
 
-    cl::NDRange global_size_row {(size_t) block_size_x*( (width_ori - 1)/(block_size_x*steps_row) + 1), \
-                                 (size_t) block_size_y*( (height_sim  - 2*offset - 1)/(block_size_y) + 1), \
+    cl::NDRange global_size_row {(size_t) block_size_x*( (width_intermed  - 1)/(block_size_x*steps_row) + 1), \
+                                 (size_t) block_size_y*( (height_intermed - 1)/(block_size_y) + 1), \
                                  (size_t) search_window_size*search_window_size };
 
     LOG(DEBUG) << "patch similarities kernel row pass";
@@ -74,16 +78,16 @@ void nlsar::compute_patch_similarities::run(cl::CommandQueue cmd_queue,
 
     kernel_col_pass.setArg(0, intermed_row_conv);
     kernel_col_pass.setArg(1, patch_similarities);
-    kernel_col_pass.setArg(2, height_sim);
-    kernel_col_pass.setArg(3, width_sim);
+    kernel_col_pass.setArg(2, height_pix);
+    kernel_col_pass.setArg(3, width_pix);
     kernel_col_pass.setArg(4, patch_size);
     kernel_col_pass.setArg(5, patch_size_max);
     // +4 is for the top and bottom halo.
     // +4 instead of +2 as for the row filter since the block_size in y direction is smaller
     kernel_col_pass.setArg(6, cl::Local((block_size_y*(steps_col+4)) * block_size_x * sizeof(float)));
 
-    cl::NDRange global_size_col {(size_t) block_size_x*( (width_ori - 1)/(block_size_x) + 1), \
-                                 (size_t) block_size_y*( (height_ori  - 1)/(block_size_y*steps_col) + 1), \
+    cl::NDRange global_size_col {(size_t) block_size_x*( (width_pat  - 1)/(block_size_x) + 1), \
+                                 (size_t) block_size_y*( (height_pat - 1)/(block_size_y*steps_col) + 1), \
                                  (size_t) search_window_size*search_window_size };
 
     LOG(DEBUG) << "patch similarities kernel column pass";
