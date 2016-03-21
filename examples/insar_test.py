af97a581365b3971bb6272e58dff5db2fb792a3a
Gerald Baier <gerald.baier@tum.de>
2016-03-21 16:20:49 +0100

    example script passes stats separately

--- examples/insar_test.py
+++ examples/insar_test.py
@@ -40,8 +40,16 @@ scale_sizes = [1, 3, 5]
 log_levels = ['debug', 'verbose', 'warning', 'fatal', 'error', 'info']
 log_levels = ['verbose', 'info', 'warning', 'fatal', 'error']
 log_levels = ['info', 'warning', 'fatal', 'error']
-training_dim = (0, 0, 25)
-methods[despeckcl.nlsar] = (search_window_size, patch_sizes, scale_sizes, training_dim, log_levels)
+nlsar_stats = despeckcl.nlsar_train(ampl_master[0:25, 0:25],
+                                    ampl_slave[0:25, 0:25],
+                                    dphase[0:25, 0:25],
+                                    patch_sizes,
+                                    scale_sizes)
+methods[despeckcl.nlsar] = (search_window_size,
+                            patch_sizes,
+                            scale_sizes,
+                            nlsar_stats,
+                            log_levels)
 
 ##################################
 #
@@ -75,13 +83,13 @@ grid = ImageGrid(fig, 111, # similar to subplot(111)
 
 # input data
 im = grid[0].imshow(20*np.log10(ampl_master), **ampl_plotopts)
-grid.cbar_axes[0].colorbar(im) 
+grid.cbar_axes[0].colorbar(im)
 
 im = grid[1].imshow(20*np.log10(ampl_slave), cmap=plt.get_cmap('gray'))
-grid.cbar_axes[1].colorbar(im) 
+grid.cbar_axes[1].colorbar(im)
 
 im = grid[2].imshow(dphase)
-grid.cbar_axes[2].colorbar(im) 
+grid.cbar_axes[2].colorbar(im)
 
 for idx, (method, args) in enumerate(methods.items()):
     print(method.__name__)
@@ -91,10 +99,10 @@ for idx, (method, args) in enumerate(methods.items()):
                                               *args)
 
     im = grid[3*(idx+1)].imshow(20*np.log10(ampl_filt), **ampl_plotopts)
-    grid.cbar_axes[3*(idx+1)].colorbar(im) 
+    grid.cbar_axes[3*(idx+1)].colorbar(im)
     im = grid[3*(idx+1) + 1].imshow(coh_filt, cmap=plt.get_cmap('gray'))
-    grid.cbar_axes[3*(idx+1) + 1].colorbar(im) 
+    grid.cbar_axes[3*(idx+1) + 1].colorbar(im)
     im = grid[3*(idx+1) + 2].imshow(dphase_filt)
-    grid.cbar_axes[3*(idx+1)+2].colorbar(im) 
+    grid.cbar_axes[3*(idx+1)+2].colorbar(im)
 
 plt.show()
