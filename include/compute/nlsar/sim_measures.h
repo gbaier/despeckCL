416768993d948acb42c826c7f5aedeb6bdde2981
Gerald Baier <gerald.baier@tum.de>
2016-07-22 16:07:30 +0200

    fixed OpenCL compiling

--- include/compute/nlsar/sim_measures.h
+++ include/compute/nlsar/sim_measures.h
@@ -16,9 +16,9 @@
  * along with despeckCL. If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #ifndef __OPENCL_VERSION__
 #include <cmath>
-using std::isnan;
 #endif
 
 
@@ -27,10 +27,14 @@ float det_covmat_2x2(float el_00, float el_01real, float el_01imag, float el_11)
     return (el_00*el_11) - (el_01real*el_01real + el_01imag*el_01imag);
 }
 
+
 float pixel_similarity_2x2(float el_00_p1, float el_01real_p1, float el_01imag_p1, float el_11_p1,
                            float el_00_p2, float el_01real_p2, float el_01imag_p2, float el_11_p2,
                            const int nlooks)
 {
+#ifndef __OPENCL_VERSION__
+  using std::isnan;
+#endif
     const int dimensions = 2;
     float nom1 = det_covmat_2x2(el_00_p1, el_01real_p1, el_01imag_p1, el_11_p1);
     float nom2 = det_covmat_2x2(el_00_p2, el_01real_p2, el_01imag_p2, el_11_p2);
