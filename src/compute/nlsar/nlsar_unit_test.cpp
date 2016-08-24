7013afb265060816eb23ebe68f75ee6a4a9a9e77
Gerald Baier <gerald.baier@tum.de>
2016-08-24 13:57:43 +0200

    disabling logging output for unit tests

--- src/compute/nlsar/nlsar_unit_test.cpp
+++ src/compute/nlsar/nlsar_unit_test.cpp
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
 
-#include "easylogging++.h"
-INITIALIZE_EASYLOGGINGPP
+#include "logging.h"
 
 int main(int argc, char **argv)
 {
+    logging_setup({});
     ::testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
     return ret;
