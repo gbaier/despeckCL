fcc81c512b9a38b73491bc876951ea172cb7a60c
Gerald Baier <gerald.baier@riken.jp>
2019-01-10 11:05:05 +0900

    added documentation to PolSAR example and checked with yapf

--- examples/polsar_test.py
+++ examples/polsar_test.py
@@ -1,107 +1,117 @@
-#!/usr/bin/python3
-
-import zipfile
-import urllib.request
-
-import numpy as np
-import matplotlib.pyplot as plt
-import gdal
+""" example the downloads some fully polarimetric data from ESA's
+PolSAR test data set and filters it with NL-SAR """
 
 import os
+import urllib.request
+import zipfile
 
-# add the build directory to the python search paths for finding the module
+import gdal
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Add build directory to the python search paths for finding the module
 # without installing it
 import sys
 sys.path.insert(0, '../build/swig/python')
 import despeckcl
 
-url = 'https://earth.esa.int/documents/653194/658149/'
+###############################
+#                             #
+# Get some test data from ESA #
+#                             #
+###############################
 
-filename = 'AIRSAR_Flevoland'
-dataname = 'FLEVOL.STK'
+URL = 'https://earth.esa.int/documents/653194/658149/'
 
-train_sub = np.s_[:, :, 200:230, 200:230]
-area_sub = np.s_[:, :, :400, :600]
-dim_sub = np.s_[:3, :3]
+FILENAME = 'AIRSAR_Flevoland'
+DATANAME = 'FLEVOL.STK'
 
+# extracts data to use for training
+TRAIN_SUB = np.s_[:, :, 200:230, 200:230]
 
-def extract_from_archive(filename, dataname):
-    with zipfile.ZipFile(filename) as zf:
-        zf.extract(dataname)
+# extracts data to be filtered and plotted
+AREA_SUB = np.s_[:, :, :400, :600]
 
 
 def stk_reader(stk_filename):
     """ see http://gdal.org/frmt_airsar.html for description """
     data = gdal.Open(stk_filename)
     data = data.ReadAsArray()
-    covmat = np.empty((3, 3, *data.shape[1:]), dtype=np.complex64)
-    covmat[0, 0] = data[0]
-    covmat[0, 1] = data[1]
-    covmat[1, 0] = data[1].conj()
-    covmat[0, 2] = data[2]
-    covmat[2, 0] = data[2].conj()
-    covmat[1, 1] = data[3]
-    covmat[1, 2] = data[4]
-    covmat[2, 1] = data[4].conj()
-    covmat[2, 2] = data[5]
-    return covmat
+    mat = np.empty((3, 3, *data.shape[1:]), dtype=np.complex64)
+    mat[0, 0] = data[0]
+    mat[0, 1] = data[1]
+    mat[1, 0] = data[1].conj()
+    mat[0, 2] = data[2]
+    mat[2, 0] = data[2].conj()
+    mat[1, 1] = data[3]
+    mat[1, 2] = data[4]
+    mat[2, 1] = data[4].conj()
+    mat[2, 2] = data[5]
+    return mat
 
 
 try:
-    covmat = stk_reader(dataname)
+    COVMAT = stk_reader(DATANAME)
 except FileNotFoundError:
-    urllib.request.urlretrieve(url + filename, filename + '.zip')
-    extract_from_archive(filename + '.zip', dataname)
-    covmat = stk_reader(dataname)
+    urllib.request.urlretrieve(URL + FILENAME, FILENAME + '.zip')
+    with zipfile.ZipFile(FILENAME + '.zip') as zf:
+        zf.extract(DATANAME)
+    COVMAT = stk_reader(DATANAME)
 
 #############
 #           #
 # Filtering #
 #           #
 #############
-covmat = covmat[dim_sub]
 
-search_window_size = 21
-patch_sizes = [3, 5, 7]
-scale_sizes = [1, 3]
-h_param = 3.0
-c_param = 49
-log_levels = ['warning', 'fatal', 'error']#, 'debug', 'info']
-
+PARAMS = {
+    'search_window_size': 21,
+    'patch_sizes': [3, 5, 7],
+    'scale_sizes': [1, 3],
+    'h': 3.0,
+    'c': 49,
+    'enabled_log_levels': ['warning', 'fatal', 'error'],  #, 'debug', 'info']
+}
 
 # store and load NL-SAR statistics
-stats_file = 'polsar_stats.txt'
+STATS_FILENAME = 'polsar_stats.txt'
 
 print('getting similarity statistics')
-if os.path.isfile(stats_file):
+if os.path.isfile(STATS_FILENAME):
     print('found saved statistics... restoring')
-    nlsar_stats = despeckcl.load_nlsar_stats_collection(stats_file)
+    NLSAR_STATS = despeckcl.load_nlsar_stats_collection(STATS_FILENAME)
 else:
     print('computing statistics')
-    nlsar_stats = despeckcl.nlsar_train(covmat[train_sub], patch_sizes, scale_sizes)
+    NLSAR_STATS = despeckcl.nlsar_train(
+        COVMAT[TRAIN_SUB], PARAMS['patch_sizes'], PARAMS['scale_sizes'])
     print('storing statistics')
-    despeckcl.store_nlsar_stats_collection(nlsar_stats, stats_file)
+    despeckcl.store_nlsar_stats_collection(NLSAR_STATS, STATS_FILENAME)
 
 print('filtering')
-covmat_filt = despeckcl.nlsar(covmat[area_sub], search_window_size,
-                              patch_sizes, scale_sizes, nlsar_stats, h_param, c_param, log_levels)
+COVMAT_FILT = despeckcl.nlsar(
+    COVMAT[AREA_SUB], nlsar_stats=NLSAR_STATS, **PARAMS)
 
-
-def plot_covmats(covmats, fig):
-    ncols = len(covmats)
-    ax = None
-    for nr, (covmat, title) in enumerate(zip(covmats, ['input', 'filtered']), 1):
-        # diagonal
-        diag = np.abs(np.diagonal(covmat)) + 0.000001
-        rgb_comp = 20 * np.log10(diag)
-        rgb_comp_norm = rgb_comp - rgb_comp.min()
-        rgb_comp_norm /= rgb_comp_norm.max()
-
-        ax = fig.add_subplot(1, ncols, nr, sharex=ax, sharey=ax)
-        ax.imshow(rgb_comp_norm)
-        ax.set_title(title)
-
+############
+#          #
+# Plotting #
+#          #
+############
 
 fig = plt.figure()
-plot_covmats([covmat[area_sub], covmat_filt], fig)
+ax = None
+
+for nr, (data, title) in enumerate(
+        zip([COVMAT[AREA_SUB], COVMAT_FILT], ['input', 'filtered']), 1):
+    # extract diagonal elements
+    diag = np.abs(np.diagonal(data)) + 0.000001
+
+    # conversion to dB and normalization
+    rgb_comp = 10 * np.log10(diag)
+    rgb_comp_norm = rgb_comp - rgb_comp.min()
+    rgb_comp_norm /= rgb_comp_norm.max()
+
+    ax = fig.add_subplot(1, 2, nr, sharex=ax, sharey=ax)
+    ax.imshow(rgb_comp_norm)
+    ax.set_title(title)
+
 plt.show()
